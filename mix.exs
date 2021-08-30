aa4f05309b4252c82519810dcd7b2ea565a2c83b
Derek Zhou <derek@3qin.us>
2021-08-30 16:44:55 -0400

    add hax package

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule Tantivy.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      # Docs
+      name: "Tantivy",
+      docs: [main: "readme", extras: ["README.md"]]
     ]
   end
 
@@ -23,7 +28,21 @@ defmodule Tantivy.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:jason, "~> 1.0"}
+      {:jason, "~> 1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    "Tantivy, an Elixir wrapper of the Rust Tantivy library using a port"
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Derek Zhou"],
+      licenses: ["Apache"],
+      links: %{"GitHub" => "https://github.com/derek-zhou/tantivy-erl"}
     ]
   end
 end
